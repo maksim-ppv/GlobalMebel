fa9822c84dcb6c17b34b9553e6cfd82f55cc0bd5
maksim-ppv <popovm1502@gmail.com>
2020-09-22 19:52:02 +0300

    Блок "о нас" и блок "бренд" с адаптивами

--- GlobalMebel/js/script.js
+++ GlobalMebel/js/script.js
@@ -84,3 +84,9 @@ $( document ).ready(function() {
     });
   };
 
+  $('.video-about__open').click(function () {
+    $("#myModal iframe").attr("src", "https://www.youtube.com/embed/Joq6QU9Fm3Y" + "?autoplay=1");
+    $('.video-about__open-video').fadeIn();
+
+});;
+
--- asrc/js/script.js
+++ asrc/js/script.js
@@ -2,4 +2,5 @@
 @@include('js_modules/preloader.js');
 @@include('js_modules/burger.js');
 @@include('js_modules/video.js');
+@@include('main.js');
 
